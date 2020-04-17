d027f9665021d69051adb8b784a78855a9de6ae8
Nathan Jansen <ndjnsn@gmail.com>
2020-04-17 12:24:43 -0400

    HW corrections
    Changed to sum within the functions rather than in main code, as per your comments

--- HW1/Harmonic_series.cpp
+++ HW1/Harmonic_series.cpp
@@ -37,15 +37,29 @@ using namespace std;
 
 //*************************************************************************
 
-float sup (int i) // function 
+
+float sup (int N) // function 
 {
-	return 1./i ;
+	float sum = 0;
+	for (int i = 1; i < N ; i++)
+	{ 
+		sum += 1./i;
+	}
+	
+	return sum;
 }
 
-float sdown (int i, int imax) //function for down summation
+float sdown (int N) //function for down summation
 {
-	return 1./(imax-i);
+	float sum = 0;
+	for (int i = 1; i < N ; i++)
+	{ 
+		sum += 1./(N-i);
+	}
+	
+	return sum;
 }
+
 
 float error (float u, float d)
 {
@@ -53,12 +67,11 @@ float error (float u, float d)
 }
 main ()
 {
-//float denom = 1;         // single precision increment = 1 (integers only)
+
 float sum_up= 0;      	// Summing up (n= 1 to N) (single precision) 
 float sum_down = 0.;     // Summing down (n= N to 1) (single precision) 
 float rel_error;
-//double sum_up_dp= 1;      	// Summing up (n= 1 to N) (double precision) 
-//double sum_down_dp = 0.;     // Summing down (n= N to 1) (double precision) 
+ 
 	  
 	ofstream fplot ("harmonic_series.dat");  // open the plot file stream for single precision
 	fplot << "# Summation of harmonic series up and down" << endl
@@ -78,12 +91,8 @@ float rel_error;
 
 for (int N = 10; N < 1000000000; N *= 10)
 { 
-	for (int i = 1; i < N ; i++)
-	{ 
-		sum_up += sup(i);
-		sum_down += sdown(i, N);
-	
-	}
+    sum_up = sup(N);
+	sum_down = sdown(N);
 	rel_error = error (sum_up, sum_down);
 	
 	fplot << left <<  scientific << setprecision(3)  << setw(18) 
@@ -94,59 +103,6 @@ for (int N = 10; N < 1000000000; N *= 10)
 	rel_error = 0;
 }
 
-//******** Summing N time fro n = 1 to N in single precision **************************************************************
-	
-/*for (float N = 1; N <= 1000000000; N *= 10)
-{
-
-	for (int i = 1; i < N ; i++)
-	{
-	  sum_up += 1./(i+1); // note sum_up starts as one so the sum is contained in this for loop
-	  sum_down += 1./(i+1);
-	}
-
-	
-	sum_down += 1;  // add 1 to the sum from n= N to n = 1
-	
-	float dif = fabs ((sum_up - sum_down) / (sum_up + sum_down));  // creates a single precision float for the difference of the two values
-	fplot << left <<  scientific << setprecision(3)  << setw(18) 
-    //<< N << "   " << setprecision(18) << dif << endl; // adds the data produced from loop into the .dat file for plotting 
-	<< log10(N) << "   " << setprecision(18) << log10(dif) << "   " << sum_up << "   " << sum_down << endl;
-	sum_down = 0; // reset sum_down to zero
-	sum_up = 1;  // resets sum_up to 1
-	
-}*/
-
-//************Out of curiosity I wanted to see the results using double precision******************
-	
-/*for (double N = 1; N <= 10; N *= 10) // Summing N time fro n = 1 to N
-{
-
-	if (N == 1)  // This is for N =1
-	{ 
-	  sum_up_dp += denom/(2);
-	  sum_down_dp += denom/(2);
-	}
-	else // this is excluding when N = 1 because I couldn't figure out another way
-	{
-		for (int i = 1; i < N ; i++)
-		{
-		  sum_up_dp += denom/(i+1); // note sum_up starts as one so the sum is contained in this for loop
-		  sum_down_dp += denom/(i+1);
-		}
-	}
-	
-	sum_down_dp += 1;  // add 1 to the sum from n= N to n = 1
-	
-	double dif_dp = fabs ((fabs(sum_up_dp) - fabs(sum_down_dp)) / (fabs(sum_up_dp) + fabs(sum_down_dp)));  // creates a double precision float for the difference of the two values
-	fplot2 << left <<  scientific << setprecision(3)  << setw(18) 
-    << N << "   " << setprecision(18) << dif_dp << "   " << sum_up_dp << "   " << sum_down_dp << endl; // adds the data produced from loop into the .dat file for plotting 
-	
-	sum_down_dp = 0; // reset sum_down_dp to zero
-	sum_up_dp = 1;  // resets sum_up_dp to 1
-	
-}*/
-	
 
 return (0);
 }
--- HW1/harmonic_series.plt
+++ HW1/harmonic_series.plt
@@ -8,12 +8,15 @@ set ylabel 'Average'
 set pointsize 1.5   # set the size of the plotted points
 set key top right    # move the key 
 set timestamp	  # turn on a date/time indicator
-plot "harmonic_series.dat" using 1:2 title 'N vs Average',\
-	a1*x + b1 title fit_title
+
 # fit the curve
 f1(x) = a1*x + b1
 fit [1:8] f1(x) "harmonic_series.dat" using ($1):($2) via a1,b1 
 fit_title = sprintf("%-+4.1f*x %-+4.1f",a1,b1)
+
+plot "harmonic_series.dat" using 1:2 title 'N vs Average',\
+	a1*x + b1 title fit_title
+
 set out "harmonic_series.ps"	# name of the output postscript file
 set terminal postscript	        # switch to postscript mode
 replot  		  	# plot to the file
